f58b5e076819cda944032bd4e6394a07f4c02e48
raj_mistry01 <makwanaraj782@gmail.com>
2024-02-09 09:27:39 +0530

    strings

---
+++ f_string.py
@@ -0,0 +1,10 @@
+# f-string means formatted strings.
+# f-string means a only one double quoatattion
+# f-string also reduced the ude of + operator
+# syntax --
+#       print(f"other strings {variable or expression}")
+# formatted strings are used to print variables or expression within single line.
+name="Raj"
+age=17
+weight=45
+print(f"My name is {name},My age is {age} and weight is {weight}")
---
+++ r_string.py
@@ -0,0 +1,4 @@
+# r_string stands for raw string.
+# all escape sequence will not work in this.
+print(f"Raj\nRaj")
+print(r"Raj\nRaj") # \n will not work bcuz it is r string type.

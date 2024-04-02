c82fd426e58bb3715e47a751ae46bd5686374ba7
raj_mistry01 <makwanaraj782@gmail.com>
2024-04-02 23:48:42 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,10 @@
+# tuple is same as list concept.
+# with list syntax ----  list_=[]
+# with tupple it is like --- tuple_=()
+# all tuple variable is a object of tuple class datatype.
+# tuples are inmutable.means constant type of list.
+# tuples can accpet duplicates.
+tuple_=(12,34,6,3,5,66)
+tuple_1=("Raj","Minali","Heyy")
+tuple_2=("raj",True,0.0,1)
+print(type(tuple_))

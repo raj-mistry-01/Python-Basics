ec86229cf6fdefe35243db21671435640778a205
raj_mistry01 <makwanaraj782@gmail.com>
2024-04-26 11:30:47 +0530

    DatatypesOfPy

---
+++ datatypes_.py
@@ -0,0 +1,21 @@
+
+# a basic concept about the datatype is that ---
+"""
+in python , all things are objects.
+so the variables are also objects.
+so the data types are the class of the python.
+then and then we can we can say that varibales are the objects of the class dataype.
+"""
+# premitive or basic datatype.
+# int 
+# float
+# string , also considered as the character array.
+# boolean
+# complex
+a=12 # int type
+b=192838362926352728765254287252428972672452897 # in python the legnth balue does not matter.
+f=12.43 # float type.
+str="I am string" # string type.
+bool1=True # boolean type.
+bool0=False # boolean type.
+comp=1+2j # complex type
